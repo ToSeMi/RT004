c14fa74ee1fa03b1754fdd6876f8495df5cb8031
Vojtech Kudibal <vojta.kudibal@gmail.com>
2023-02-21 16:07:59 +0100

    done step01 and step02

--- src/rt004-NET6/Program.cs
+++ src/rt004-NET6/Program.cs
@@ -1,24 +1,67 @@
 ﻿using Util;
+using System.Text.Json;
+using System.IO;
 //using System.Numerics;
 
 namespace rt004;
+
+[Serializable]
+class Params {
+  public int width {set;get;}
+  public int height {set;get;}
+  public string fileName {set;get;}
+  public Params(int width, int height, string fileName) {
+    this.fileName = fileName;
+    this.height = height;
+    this.width = width;
+  }
+}
+class JsonParser {
+  string filename;
+  public JsonParser(string filename){
+    this.filename = filename;
+  }
+
+  public void ReadFile(out int wid, out int hei, out string fname){
+    //default params
+    wid = 600;
+    hei = 450;
+    fname = "demo.pfm";
+    try {
+        string sr =File.ReadAllText(filename);
+        Params? par = JsonSerializer.Deserialize<Params>(sr);
+        
+        wid = par.width;
+        hei = par.height;
+        fname = par.fileName!;
+        
+      
+    }catch(IOException e) {
+      System.Console.WriteLine(e);
+    }
+  }
+};
 
 internal class Program
 {
   static void Main(string[] args)
   {
     // Parameters.
-    // TODO: parse command-line arguments and/or your config file.
-    int wid = 600;
-    int hei = 450;
-    string fileName = "demo.pfm";
-
+    int wid, hei;
+    string fileName;
+    string configFileName = "conf.json";
+    if(args.Length == 1){
+      configFileName = args[0];
+    }
+    JsonParser jp = new JsonParser(configFileName);
+    jp.ReadFile(out wid, out hei, out fileName);
+    
     // HDR image.
     FloatImage fi = new FloatImage(wid, hei, 3);
 
     // TODO: put anything interesting into the image.
     // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
-
+    fi.PutPixel(2,5,new float[]{5f, 4f, 255f});
     //fi.SaveHDR(fileName);   // Doesn't work well yet...
     fi.SavePFM(fileName);
 
