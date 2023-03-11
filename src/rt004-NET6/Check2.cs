ead618223e6262ef17a6453c049c86495c28f687
Vojtech Kudibal <vojta.kudibal@gmail.com>
2023-03-11 09:44:29 +0100

    Adding materials and lights

--- src/rt004-NET6/Check2.cs
+++ src/rt004-NET6/Check2.cs
@@ -4,9 +4,10 @@ using System.Numerics;
 
 namespace rt004.checkpoint2 {
 	
-	class Scene {
-		FloatImage? fi;
+	public class Scene {
+		FloatImage fi;
 		List<Solid> objects;
+		List<LightSrc> lights;
 		FloatCamera camera;
 		string filename;
 
@@ -14,12 +15,19 @@ namespace rt004.checkpoint2 {
 			fi = new FloatImage(wid, hei, 3);
 			this.filename = filename;
 			objects = new();
+			lights = new();
 			this.camera = fc;
 		}
 
 		public void AddSolid(Solid s) {
 			objects.Add(s);
 		}
+
+		public void AddLight(LightSrc light){
+			lights.Add(light);
+		}
+
+
 
 		public Scene? LoadScene(string fileName){
 			Scene? returnable = null;
@@ -28,64 +36,134 @@ namespace rt004.checkpoint2 {
 		}
 	}
 
-	abstract class ObjectOnMap {
-		protected Vector3 position;
-		protected Scene? scene;
-		public abstract void Draw();
-	}
-	abstract class Solid : ObjectOnMap {
-		protected Vector2 TxtCoords;
-		public override void Draw() {
-			throw new NotImplementedException();
+	public class Phong {
+		Vector3 normal,light,view, color;
+		public readonly float H = 0.5479f;
+		public readonly float kA = 0.3f, kD = 0.3f, kS = 0.4f; // kA + kD + kS = 1
+
+
+		public Phong(Vector3 n, Vector3 l, Vector3 v, Vector3 color, float highlight,float kA, float kD, float kS){
+			normal = n; 
+			light = l;
+			view = v;
+			this.kA = kA;
+			this.kD = kD;
+			this.kS = kS;
+			this.H = highlight;
+			this.color = color;
 		}
+		public Vector3 DiffuseComponent( ){
+			return  normal*color*kD * Vector3.Dot(view,normal);
+		}
+		public Vector3 AmbientLight(){
+			return color*kA;
+		}
+		public Vector3 SpecularComponent(Vector3 unitV, Vector3 unitR){
+			return light*color*kS*(float)Math.Pow(Vector3.Dot(unitR,unitV),H);
+		}
+		
+	}
+	public abstract class ObjectOnMap {
+		protected Vector3 position;
+		public Vector3 Position {get => position; set=> position = value;}
+		protected Scene? scene;
+	}
+	public abstract class Solid : ObjectOnMap {
+		public abstract void Draw();
 
 		public abstract bool Intersect(FloatCamera fc);
 	}
 
-	class LightSrc : ObjectOnMap {
-		public override void Draw(){
-			throw new NotImplementedException();
+	public abstract class LightSrc  {
+		protected Vector3 Intensity;
+		protected Phong? model;
+	}
+
+	public class PointLightSource : LightSrc {
+
+	}
+
+	public class DirectionLightSource : LightSrc {
+		Vector3 Direction;
+		public void shade() {
+			var E = model?.AmbientLight() 
+			+ model?.DiffuseComponent() 
+			+ model?.SpecularComponent(Vector3.UnitX, Vector3.UnitY); 
 		}
 	}
 	/*
 	I want to have a perspective camera
 	*/
-    class FloatCamera : ObjectOnMap {
+    public class FloatCamera : ObjectOnMap {
+		Matrix4x4 projection;
         Vector3 direction;
 		float frustrum;
+
+		public Vector3 Direction {get => direction; set=> position = value;}
+		public float Frustrum {get=>frustrum; set=> frustrum=value;}
 
 		public FloatCamera(float x = 0,float y = 0, float z = 0, float a1 =0, float a2=0, float a3 =0, float frustrum =0){
 			this.direction = new Vector3(x,y,z);
 			this.position = new Vector3(a1,a2,a3);
 			this.frustrum = frustrum;
+			projection = new Matrix4x4(	1,0,0,0,
+										0,1,0,0,
+										0,0,1,1/frustrum,
+										0,0,0,0);
 		}
 		public FloatCamera(Vector3 pos, Vector3 dir, float frustrum = 0){
 			this.position = pos;
 			this.direction = dir;
 			this.frustrum = frustrum;
 		}
+
+
+    }
+
+    public class Plane : Solid
+    {
         public override void Draw()
         {
             throw new NotImplementedException();
         }
 
-    }
-	
-	class Sphere : Solid {
-		float radius;
-
         public override bool Intersect(FloatCamera fc)
+        {
+			var D = Vector3.Distance(fc.Position,this.Position);
+
+			var p0 = fc.Position;
+			var p1 = fc.Direction;
+			var n = Position;
+			var t= -1*(Vector3.Dot(n , p0) + D) / (Vector3.Dot(n,p1));
+			return isinplane(p0 + t*p1,D);
+			
+        }
+		bool isinplane(Vector3 v, float D){
+			return Math.Abs(v.X*Position.X + v.Y * Position.Y + v.Z* Position.Z - D)  <= 10e-9;
+		}
+
+    }
+
+    public class Sphere : Solid {
+        public override void Draw()
         {
             throw new NotImplementedException();
         }
-    }
-	//geometric tools
-	class Cube : Solid {
-		float side;
 
         public override bool Intersect(FloatCamera fc)
         {
-            throw new NotImplementedException();
+
+			
+			var D = Vector3.Distance(fc.Position,this.Position);
+
+			var p0 = fc.Position;
+			var p1 = fc.Direction;
+			var n = Position;
+			var t= -1*(Vector3.Dot(n , p0) + D) / (Vector3.Dot(n,p1));
+			return Math.Abs(t*t*(Vector3.Dot(p1,p1)) +2*t*(Vector3.Dot(p0,p1) + Vector3.Dot(p0,p1)) - D) <= 10e-9;
+
         }
+
     }
+
 }
